637f310ae3baca895830c3c1ba2fe2f910507a9f
Thierry Jefersson <thierryjefersson@gmail.com>
2023-03-08 21:08:43 -0300

    perguntas-frequentes

--- js/script.js
+++ js/script.js
@@ -1,11 +1,42 @@
+// Ativar Links do Menu
 const links = document.querySelectorAll(".header-menu a");
 
 function ativarLink(link) {
-  const url = document.location.href
-  const href = link.href
+  const url = document.location.href;
+  const href = link.href;
   if (url.includes(href)) {
-    link.classList.add('ativo');
+    link.classList.add("ativo");
   }
 }
 
 links.forEach(ativarLink);
+
+// Ativar Items do Orçamento
+const parametros = new URLSearchParams(location.search);
+
+function ativarProduto(parametro) {
+  const elemento = document.getElementById(parametro);
+  if (elemento) {
+    elemento.checked = true;
+  }
+}
+
+parametros.forEach(ativarProduto);
+
+// Perguntas Frequentes
+const perguntas = document.querySelectorAll(".perguntas button");
+
+function ativarPergunta(event){
+  const pergunta = event.currentTarget;
+  const controls = pergunta.getAttribute('aria-controls');
+  const resposta = document.getElementById(controls);
+  pergunta.setAttribute('aria-expanded', 'true')
+  resposta.classList.toggle('ativa');
+  console.log(resposta);
+}
+
+function eventosPerguntas(pergunta) {
+  pergunta.addEventListener('click', ativarPergunta)
+}
+
+perguntas.forEach(eventosPerguntas)
