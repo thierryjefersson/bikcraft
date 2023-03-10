5449d2779e90cee510eb6fbaf7e062c430f5bc43
Thierry Jefersson <thierryjefersson@gmail.com>
2023-03-09 21:33:56 -0300

    js-biciclets

--- js/script.js
+++ js/script.js
@@ -26,17 +26,38 @@ parametros.forEach(ativarProduto);
 // Perguntas Frequentes
 const perguntas = document.querySelectorAll(".perguntas button");
 
-function ativarPergunta(event){
+function ativarPergunta(event) {
   const pergunta = event.currentTarget;
-  const controls = pergunta.getAttribute('aria-controls');
+  const controls = pergunta.getAttribute("aria-controls");
   const resposta = document.getElementById(controls);
-  pergunta.setAttribute('aria-expanded', 'true')
-  resposta.classList.toggle('ativa');
-  console.log(resposta);
+
+  resposta.classList.toggle("ativa");
+  const ativa = resposta.classList.contains("ativa");
+  console.log(ativa)
+  pergunta.setAttribute("aria-expanded", ativa);
+
 }
 
 function eventosPerguntas(pergunta) {
-  pergunta.addEventListener('click', ativarPergunta)
+  pergunta.addEventListener("click", ativarPergunta);
 }
 
-perguntas.forEach(eventosPerguntas)
+perguntas.forEach(eventosPerguntas);
+
+// Galeria de Bicicletas
+const galeria = document.querySelectorAll(".bicicleta-imagens img")
+const galeriaContainer = document.querySelector(".bicicleta-imagens")
+
+function trocarImagem(event) {
+  const img = event.currentTarget;
+  const media = matchMedia("(min-width: 1000px)").matches;
+  if (media) {
+  galeriaContainer.prepend(img)
+}
+}
+
+function eventosGaleria(img) {
+img.addEventListener('click', trocarImagem)
+}
+
+galeria.forEach(eventosGaleria)
